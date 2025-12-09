f625907c362899c7ffee12b292b717393f28d1fc
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-12-09 11:40:40 +0330

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import DashBoard from "./pages/DashBoard";
+import { Link, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      
+      <nav style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">SignUp</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </nav>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
