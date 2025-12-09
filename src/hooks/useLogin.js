3d89cec1600df983d4db2fc5b27da7cb7286ee30
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-12-09 14:41:42 +0330

    profile refetcing using querykey on useLogin

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,19 +1,24 @@
 // src/hooks/useLogin.js
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { authApi } from "../api/authApi";
 import { useAuth } from "../context/AuthContext";
 
 export function useLogin() {
   const { login } = useAuth();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (data) => authApi.login(data),
+
     onSuccess: (res) => {
-      // Adjust this based on your backend response structure
       const token = res?.data?.token;
 
       if (token) {
+        // Save token in auth context + localStorage + axios header
         login(token);
+
+        // Refetch profile query after login
+        queryClient.invalidateQueries({ queryKey: ["profile"] });
       } else {
         console.warn("No token found in login response");
       }
