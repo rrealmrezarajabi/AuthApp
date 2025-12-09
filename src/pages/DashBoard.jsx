7f22d975c7d6cfbe40e92c3ace403af3e49e5882
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-12-09 14:34:08 +0330

    dashboard added but when user is authenticated

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
-const DashBoard = () => {
+function DashboardPage() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
   return (
-    <div>
-      
+    <div style={{ maxWidth: "600px", margin: "0 auto" }}>
+      <h2>Dashboard</h2>
+      <p>This is a protected page. Only logged-in users should see this.</p>
+
+      <button onClick={handleLogout}>Logout</button>
     </div>
-  )
+  );
 }
 
-export default DashBoard
+export default DashboardPage;
