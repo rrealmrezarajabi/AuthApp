ca3975c99a352e5d81deb28da2c4ee5d91f0323e
rrealmrezarajabi <rrealmrezarajabi@gmail.com>
2025-12-09 14:39:28 +0330

    useProfile hook added

---
+++ src/hooks/useProfile.js
@@ -0,0 +1,17 @@
+// src/hooks/useProfile.js
+import { useQuery } from "@tanstack/react-query";
+import { authApi } from "../api/authApi";
+import { useAuth } from "../context/AuthContext";
+
+export function useProfile() {
+  const { token } = useAuth();
+
+  return useQuery({
+    queryKey: ["profile"],
+    queryFn: async () => {
+      const res = await authApi.getProfile();
+      return res.data;
+    },
+    enabled: !!token, // only run when user is authenticated
+  });
+}
